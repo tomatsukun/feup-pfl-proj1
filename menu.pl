27bf639090d5a697079dc805a163e20c7f27d444
tomatsukun <tmsmcdpssl@gmail.com>
2023-10-31 03:27:35 +0000

    Bot facil já esta a colocar a peça de forma random

--- menu.pl
+++ menu.pl
@@ -7,6 +7,7 @@
 
 % Bibliotecas necessárias
 :- use_module(library(lists)).
+:- use_module(library(random)).
 
 % --------------------- MENU PRINCIPAL ---------------------
 
@@ -33,6 +34,50 @@ process_option(1) :- % Humano vs Humano
     clear,
     choose_move(Board).
 
+process_option(2) :- % Humano vs Computador
+    initial_board(Board),
+    clear,
+    choose_bot_dif(Board).
+    
+
+
+choose_bot_dif(Board):-
+    write('|-----------------------------|'), nl,
+    write('|    Choose Bot Difficulty    |'), nl,
+    write('|-----------------------------|'), nl,
+    write('| 1. Easy                     |'), nl,
+    write('| 2. Hard                     |'), nl,
+    write('|-----------------------------|'), nl,
+    write('| Escolha uma opcao: '),
+    read(Option),
+    process_choose_bot_dif(Option,Board).
+
+process_choose_bot_dif(1,Board):-
+    clear,
+    %%choose_move(Board), % turno do player
+    random(1, 3, RandNumber), % escolhe random se vai mover ou clocar nova peça
+    random(0, 6, Rand_X),
+    random(0, 6, Rand_Y),
+    ( 1 = 1 -> place_piece_bot(Rand_X-Rand_Y,Board) ; write('Bot moves piece') ).
+    
+
+
+place_piece_bot(X-Y, Board):-
+    write('New piece cords '),
+    write('('),
+    write(X), write(','), write(Y),
+    write(')'), nl,
+
+    run(r,X, Y, Board, UpdatedBoard, Counter, NewCounter),
+    continues(NewCounter, UpdatedBoard).
+  
+continues(NewCounter, Board):-
+    write('Do you want to place another piece? (yes/no)'), nl,
+    read(Answer),
+    (Answer = yes -> place_piece(NewCounter, Board); true).
+
+
+
 
 choose_move(Board):-
     write('|-----------------------------|'), nl,
@@ -48,15 +93,14 @@ choose_move(Board):-
 
 %% Player chose to place a piece
 process_choose_move(1, Board):-
-      clear,
-      place_piece(8, Board).
-
-
+    clear,
+    place_piece(8, Board).
 
 %% Player chose to move a already existing piece
 process_choose_move(2):-
-      clear,
-      move_piece.
+    clear,
+    %move_piece.
+    write('Escolheu move piece.').
 
 
 %% Place piece 
@@ -66,19 +110,20 @@ place_piece(0, Board) :-
     write('|-----------------------------|'), nl, nl,  
     display_board(Board).
 place_piece(Counter, Board):-
-  Counter > 0,
-  write('|-----------------------------|'), nl,
-  write('|    Choose Where to place    |'), nl,
-  write('|-----------------------------|'), nl,
-  write('| X-Y of the piece |'), nl,
-  read(X-Y),
+    Counter > 0,
+    write('|-----------------------------|'), nl,
+    write('|    Choose Where to place    |'), nl,
+    write('|-----------------------------|'), nl,
+    write('| X-Y of the piece |'), nl,
+    read(X-Y),
 
-  write('New piece cords '),
-  write('('),
-  write(X), write(','), write(Y),
-  write(')'), nl,
-  run(r,X, Y, Board, UpdatedBoard, Counter, NewCounter),
-  continues(NewCounter, UpdatedBoard).
+    write('New piece cords '),
+    write('('),
+    write(X), write(','), write(Y),
+    write(')'), nl,
+
+    run(r,X, Y, Board, UpdatedBoard, Counter, NewCounter),
+    continues(NewCounter, UpdatedBoard).
   
 continues(NewCounter, Board):-
     write('Do you want to place another piece? (yes/no)'), nl,
